29a4a1b0eb95dd01cc835c6abb110f46e6cabe4a
Aditya Pandey <ap41368@gmail.com>
2022-09-27 16:05:55 +0530

    about page updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-export default function About(){
-    return(
-        <div>
-            <h1>Git Finder</h1>
-            <p>This app is about Git users</p>
-            <p>Version 1.0.0</p>
-        </div>
-    )
+const About = () => {
+  return (
+    <div>
+        <h1>Git Finder</h1>
+        <p>
+            Use this app to search users on Github.
+        </p>
+        <p>Version: 1.0.0</p>
+    </div>
+  )
 }
+
+export default About;
